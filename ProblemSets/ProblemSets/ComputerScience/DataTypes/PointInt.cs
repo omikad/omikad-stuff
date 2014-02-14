c50cd948c800887fe656cb14d13b6081ded92db3
omikad <omikad@gmail.com>
2014-02-14 10:56:22 +0400

    Find Largest Rectangle in 2D solved

---
+++ ProblemSets/ProblemSets/ComputerScience/DataTypes/PointInt.cs
@@ -0,0 +1,36 @@
+﻿namespace ProblemSets.ComputerScience.DataTypes
+{
+	public struct PointStruct<T>
+	{
+		public T X;
+		public T Y;
+
+		public PointStruct(T x, T y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public override string ToString()
+		{
+			return new { X, Y }.ToString();
+		}
+	}
+
+	public class RectangleClass<T>
+	{
+		public PointStruct<T> LeftUp;
+		public PointStruct<T> RightDown;
+
+		public RectangleClass(PointStruct<T> leftUp, PointStruct<T> rightDown)
+		{
+			LeftUp = leftUp;
+			RightDown = rightDown;
+		}
+
+		public override string ToString()
+		{
+			return new { LeftUp, RightDown }.ToString();
+		}
+	}
+}
--- ProblemSets/ProblemSets/Problems/FindLargestRectangleIn2D.cs
+++ ProblemSets/ProblemSets/Problems/FindLargestRectangleIn2D.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
+using ProblemSets.ComputerScience.DataTypes;
 using ProblemSets.Services;
 
 namespace ProblemSets.Problems
@@ -12,64 +14,133 @@ namespace ProblemSets.Problems
 		{
 			var rect = new[]
 			{
-				"100010101011111111111",
+				"110010101001111111111",
+				"100011111010110001111",
 				"100011111010000001111",
-				"101011111010000001111",
-				"100011111010000001111",
-				"100010101010000101110",
-				"100010101010000001111",
+				"100011111010000001101",
+				"000010101010000101111",
+				"110010101010000000111",
+				"100010101010000000111",
+				"100010101010000000111",
 			};
 			Solve(rect.ToTwoDimensional(s => s));
 		}
 
 		private static void Solve(char[,] rect)
 		{
+			// Let the size of the rectangle is N width, M height
+			// Overall solution: time O(N*M), memory O(N) 
+
+			// Search largest rectangle in the first top line - O(N)
+			// Hold heights of the same chars above the line - memory O(N)
+			// Outer cycle by lines - M iterations
+			//		Update heights array - N iterations
+			//		Search rectangles of '0' and '1' above the line
+			//			Use stack of the increasing heights - memory O(N)
+			//			When the height is decreased (or line ended) - calculate largest rectangle
+			//			Every cell can be pushed into the stack once, and popped once => total time complexity is linear O(N)
+
 			rect.Print();
 
-			var solutionX = new int[rect.GetLength(0), rect.GetLength(1)];
-			var solutionY = new int[rect.GetLength(0), rect.GetLength(1)];
+			var width = rect.GetLength(1);
 
-			var maxY = rect.GetLength(0);
-			var maxX = rect.GetLength(1);
+			var best = FindBestFirstLine(rect);
+			Console.WriteLine(best);
 
-			for (var i = maxY - 1; i >= 0; i--)
+			var heights = Enumerable.Repeat(1, width).ToArray();
+
+			for (var line = 1; line < rect.GetLength(0); line++)
 			{
-				solutionX[i, maxX - 1] = maxX - 1;
-				solutionY[i, maxX - 1] =
-					i == maxY - 1 || rect[i + 1, maxX - 1] != rect[i, maxX - 1]
-						? i
-						: solutionY[i + 1, maxX - 1];
-			}
-
-			for (var j = maxX - 1; j >= 0; j--)
-			{
-				solutionY[maxY - 1, j] = maxY - 1;
-				solutionX[maxY - 1, j] =
-					j == maxX - 1 || rect[maxY - 1, j + 1] != rect[maxY - 1, j]
-						? j
-						: solutionX[maxY - 1, j + 1];
-			}
-
-			for (var i = maxY - 1; i >= 0; i--)
-			{
-				for (var j = maxX - 1; j >= 0; j--)
+				for (var i = 0; i < width; i++)
 				{
-					
+					if (rect[line - 1, i] == rect[line, i])
+						heights[i]++;
+					else
+						heights[i] = 1;
+				}
+
+				FindAboveLine(rect, heights, line, '0', best);
+				FindAboveLine(rect, heights, line, '1', best);
+
+				Console.WriteLine(best);
+			}
+
+			Console.WriteLine();
+			Console.WriteLine(best);
+		}
+
+		private static RectangleClass<int> FindBestFirstLine(char[,] rect)
+		{
+			// O(N)
+
+			var bestLu = new PointStruct<int>(0, 0);
+			var bestRd = new PointStruct<int>(0, 0);
+
+			var curWidth = 1;
+			for (var i = 1; i < rect.GetLength(1); i++)
+			{
+				if (rect[0, i] == rect[0, i - 1])
+				{
+					curWidth++;
+					if (curWidth > bestRd.X - bestLu.X + 1)
+					{
+						bestLu = new PointStruct<int>(i - curWidth + 1, 0);
+						bestRd = new PointStruct<int>(i, 0);
+					}
+				}
+				else curWidth = 1;
+			}
+
+			return new RectangleClass<int>(bestLu, bestRd);
+		}
+
+		private static void FindAboveLine(char[,] rect, int[] heights, int line, char target, RectangleClass<int> best)
+		{
+			var stack = new Stack<PointStruct<int>>();
+
+			var bestArea = (best.RightDown.X - best.LeftUp.X + 1) * (best.RightDown.Y - best.LeftUp.Y + 1);
+			Console.WriteLine(new { line, target, bestArea });
+
+			for (var i = 0; i < rect.GetLength(1); i++)
+			{
+				var curHeight = rect[line, i] == target ? heights[i] : 0;
+
+				if (stack.Count == 0)
+				{
+					if (curHeight > 0)
+						stack.Push(new PointStruct<int>(i, curHeight));
+				}
+				else
+				{
+					FinishSlope(stack, curHeight, line, i, ref bestArea, best);
+
+					if (stack.Count == 0 || curHeight > stack.Peek().Y)
+						stack.Push(new PointStruct<int>(i, curHeight));
 				}
 			}
 
-
-			Console.WriteLine("solutionX:");
-			Print(solutionX);
-			Console.WriteLine("solutionY:");
-			Print(solutionY);
+			FinishSlope(stack, 0, line, rect.GetLength(1), ref bestArea, best);
 		}
 
-		private static void Print(int[,] arr)
+		private static void FinishSlope(Stack<PointStruct<int>> stack,
+		                                int curHeight,
+										int line,
+										int i,
+		                                ref int bestArea,
+		                                RectangleClass<int> best)
 		{
-			foreach (var row in arr.EnumerateRows())
-				Console.WriteLine(" ".Join(row.Select(i => string.Format("{0,2}", i))));
-			Console.WriteLine();
+			while (stack.Count > 0 && stack.Peek().Y > curHeight)
+			{
+				var endedRectLu = stack.Pop();
+				var endedArea = (i - endedRectLu.X) * endedRectLu.Y;
+				if (endedArea > bestArea)
+				{
+					best.LeftUp = new PointStruct<int>(endedRectLu.X, line - endedRectLu.Y + 1);
+					best.RightDown = new PointStruct<int>(i - 1, line);
+					bestArea = endedArea;
+					Console.WriteLine(new { bestArea });
+				}
+			}
 		}
 	}
 }
