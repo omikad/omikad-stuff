ffa1dedca319133f1d05ee3b858e259a2e6b23d4
omikad <omikad>
2014-02-25 17:01:13 +0400

    Add two numbers without arithmetic; Count numbers of 2

---
+++ ProblemSets/ProblemSets/Problems/AddTwoNumbers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace ProblemSets.Problems
+{
+	[Export]
+	public class AddTwoNumbers
+	{
+		public void Go()
+		{
+			// Add two numbers. + or any other arithmetic operators are not allowed
+
+			Add(0, 0);
+			Add(1, 0);
+			Add(3, 0);
+			Add(0, 3);
+			Add(7, 3);
+			Add(16, 7);
+			Add(123, 77);
+		}
+
+		private static void Add(int x, int y)
+		{
+			// TODO: support negative numbers: use formula -x = !x + 1
+
+			var sum = 0;
+			var carry = 0;
+			var mul = 1;
+
+			while (x != 0 || y != 0)
+			{
+				var lsbx = x & 1;
+				var lsby = y & 1;
+
+				if (carry == 1 && lsbx == 1 && lsby == 1)
+				{
+					sum |= mul;
+				}
+				else 
+				{
+					if (carry == 1)
+					{
+						if (lsbx == 0) lsbx = 1; else lsby = 1;
+					}
+
+					if ((lsbx ^ lsby) == 1)
+					{
+						sum |= mul;
+					}
+					
+					carry = lsbx & lsby;
+				}
+
+				mul <<= 1;
+				x >>= 1;
+				y >>= 1;
+			}
+
+			if (carry == 1)
+				sum |= mul;
+
+			Console.WriteLine(new { x, y, sum });
+			Console.WriteLine();
+		}
+	}
+}
---
+++ ProblemSets/ProblemSets/Problems/CountNumberOf2s.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace ProblemSets.Problems
+{
+	[Export]
+	public class CountNumberOf2s
+	{
+		public void Go()
+		{
+			Console.WriteLine(new { n = 125, result = Solve(125), brute = Solve_Brute(125) });
+
+			for (var i = 0; i < 2000; i++)
+			{
+				var result = Solve(i);
+				var brute = Solve_Brute(i);
+
+				if (brute != result)
+					throw new InvalidOperationException();
+			}
+
+			Console.WriteLine("Passed!");
+		}
+
+		private static int Solve(int n)
+		{
+			var cnt = 0;
+			var mul = 1;
+			var nn = n;
+
+			while (nn > 0)
+			{
+				var decs = nn / 10;
+
+				if (nn % 10 > 2)
+					decs++;
+
+				cnt += decs * mul;
+
+				if (nn % 10 == 2)
+					cnt += n - nn * mul + 1;
+
+				nn /= 10;
+				mul *= 10;
+			}
+
+			return cnt;
+		}
+
+		private static int Solve_Brute(int n)
+		{
+			var cnt = 0;
+			for (var i = 2; i <= n; i++)
+				cnt += i.ToString().Count(c => c == '2');
+			return cnt;
+		}
+	}
+}
